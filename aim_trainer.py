0b9d79cba388ade06e0534f47b4bcba98f2f0ab6
Patrick Brennan <padist3r@gmail.com>
2020-10-22 15:35:02 +0100

    Added a target. have to figure out how to remove it permanently when clicked on

--- aim_trainer.py
+++ aim_trainer.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 # Colours to use
 black = (0, 0, 0)
@@ -19,9 +20,14 @@ pygame.display.set_icon(icon)
 # Player Sights
 player_sights = pygame.image.load("Images\\sight.png")  # 24px
 
+# Target
+load_target = pygame.image.load("Images\\bullseye.png")     # 128px
+target_x = 500
+target_y = 500
 
 # Hides the OS mouse
 pygame.mouse.set_visible(False)
+mouse_click = False
 
 
 def player(x, y):
@@ -30,21 +36,45 @@ def player(x, y):
     screen.blit(player_sights, (x - 12, y - 12))
 
 
+def draw_target(x, y):
+    """Draws the targets"""
+    screen.blit(load_target, (x - 64, y - 64))
+
+
+def is_collision(px, py, tx, ty):
+    distance = math.sqrt(((tx - px) ** 2) + ((ty - py) ** 2))
+    if distance < 65:
+        return True
+    else:
+        return False
+
+
 # Game loop
 running = True
 while running:
+
+    mouse_x, mouse_y = pygame.mouse.get_pos()
 
     # Checks for player inputs and button presses
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    mouse_x, mouse_y = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_click = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            mouse_click = False
 
     # What gets drawn on the screen (order matters)
     screen.fill(white)
+
+    # This checks to see if you have clicked on the target
+    # I still need to figure out how to fully remove target and redraw a new one
+    if is_collision(mouse_x, mouse_y, target_x, target_y) and mouse_click:
+        pass
+    else:
+        draw_target(target_x, target_y)
+
     player(mouse_x, mouse_y)
     pygame.display.update()
-
 
 pygame.quit()
